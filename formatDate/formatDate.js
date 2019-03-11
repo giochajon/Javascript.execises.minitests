ed9483b2be978be8fa8deaa54864ad589e44422c
Giovanni Chajon <giochajon@gmail.com>
2019-03-11 14:37:10 -0600

    added the excercise for monday march 11 2019

---
+++ formatDate/formatDate.js
@@ -0,0 +1,25 @@
+
+/*
+
+
+
+Write a function that converts user entered date formatted as M/D/YYYY to a format required by an API (YYYYMMDD). The parameter "userDate" and the return value are strings.
+
+For example, it should convert user entered date "12/31/2014" to "20141231" suitable for the API.
+
+
+*/
+
+
+function AddLeadingZero(num) {
+  return (num < 10 ? '0' : '') + num;
+}
+
+function formatDate(userDate) {
+  // format from M/D/YYYY to YYYYMMDD
+  var [month, day, year] = userDate.split('/');
+  return year + AddLeadingZero(month) + AddLeadingZero(day); 
+}
+
+
+console.log(formatDate("12/31/2014"));
