e87f84d98f0b9d5ec602aa9aacc261cb79c7875b
Giovanni Chajon <giochajon@gmail.com>
2019-03-14 18:19:56 -0600

    added the gallery excercise march 14 2019

---
+++ functions and braces nightmare/fb.js
@@ -0,0 +1,24 @@
+console.log("Hello from exercises");
+
+const array1 = [3, 1];
+
+// write the function that will make this work
+
+function orderArray(inpArray) {
+	let a = inpArray[0];
+	let b = inpArray[1];
+	let c = [];
+	if (a > b) {
+		c = [b, a];
+	} else {
+		c = [a, b];
+	}
+
+	return c;
+}
+
+console.log("Should Be: 1, 3:", orderArray(array1));
+
+console.log("Should Be: 1, 5:", orderArray([1, 5]));
+
+console.log("Should Be: 10, 20:", orderArray([20, 10]));
